2d7d38f6bf75ac46290f684d9d76999e400320f4
deniskhan22bd <odinstrelok02@gmail.com>
2024-04-29 14:42:45 +0600

    add docker

--- cmd/main.go
+++ cmd/main.go
@@ -3,17 +3,23 @@ package main
 import (
 	"database/sql"
 	"flag"
-	"github.com/deniskhan22bd/Golang/ProjectGolang/pkg/jsonlog"
-	"github.com/deniskhan22bd/Golang/ProjectGolang/pkg/models"
+	"fmt"
 	"os"
 
+	"github.com/deniskhan22bd/Golang/ProjectGolang/pkg/jsonlog"
+	"github.com/deniskhan22bd/Golang/ProjectGolang/pkg/models"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/lib/pq"
+	"github.com/peterbourgon/ff/v3"
 )
 
 type config struct {
-	port int
-	env  string
-	db   struct {
+	port       int
+	env        string
+	migrations string
+	db         struct {
 		dsn string
 	}
 }
@@ -25,14 +31,33 @@ type application struct {
 }
 
 func main() {
-	var cfg config
-	flag.IntVar(&cfg.port, "port", 8080, "API server port")
-	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
-	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://postgres:123@localhost/golang_project?sslmode=disable", "PostgreSQL DSN")
-	flag.Parse()
+	fs := flag.NewFlagSet("demo-app", flag.ContinueOnError)
 
+	var (
+		cfg        config
+		migrations = fs.String("migrations", "file://pkg/migrations", "Path to migration files folder. If not provided, migrations do not applied")
+		port       = fs.Int("port", 8080, "API server port")
+		env        = fs.String("env", "development", "Environment (development|staging|production)")
+		dbDsn      = fs.String("dsn", "postgres://postgres:123@localhost:5432/golang_project?sslmode=disable", "PostgreSQL DSN")
+	)
 	// Init logger
 	logger := jsonlog.NewLogger(os.Stdout, jsonlog.LevelInfo)
+	if err := ff.Parse(fs, os.Args[1:], ff.WithEnvVars()); err != nil {
+		logger.PrintFatal(err, nil)
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+	}
+
+	cfg.port = *port
+	cfg.env = *env
+	cfg.db.dsn = *dbDsn
+	cfg.migrations = *migrations
+
+	logger.PrintInfo("starting application with configuration", map[string]string{
+		"port":       fmt.Sprintf("%d", cfg.port),
+		"env":        cfg.env,
+		"db":         cfg.db.dsn,
+		"migrations": cfg.migrations,
+	})
 
 	// Connect to DB
 	db, err := openDB(cfg)
@@ -65,5 +90,20 @@ func openDB(cfg config) (*sql.DB, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	if cfg.migrations != "" {
+		driver, err := postgres.WithInstance(db, &postgres.Config{})
+		if err != nil {
+			return nil, err
+		}
+		m, err := migrate.NewWithDatabaseInstance(
+			cfg.migrations,
+			"postgres", driver)
+		if err != nil {
+			return nil, err
+		}
+		m.Up()
+	}
+
 	return db, nil
 }
--- go.mod
+++ go.mod
@@ -7,4 +7,14 @@ require (
 	github.com/lib/pq v1.10.9
 )
 
-require golang.org/x/crypto v0.21.0
+require (
+	github.com/golang-migrate/migrate/v4 v4.17.1
+	github.com/peterbourgon/ff/v3 v3.4.0
+	golang.org/x/crypto v0.21.0
+)
+
+require (
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+)
